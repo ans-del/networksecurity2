f401f353ec2805f1c77ce312445a2ba718b8492d
Anshika Saroj <anshikasaroj139@gmail.com>
2026-02-01 09:14:56 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity2.components.data_ingestion import DataIngestion
 from networksecurity2.components.data_validation import DataValidation
+from networksecurity2.components.data_transformation import DataTransformation
 from networksecurity2.exception.exception import NetworkSecurityException
 from networksecurity2.logging.logger import logging
-from networksecurity2.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity2.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity2.entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -22,6 +23,12 @@ if __name__=='__main__':
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("data Validation completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelinfconfig)
+        logging.info("data Transformation started")
+        data_transformaton = DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation_artifact = data_transformaton.initiate_data_transformation()
+        logging.info("data Transformation completed")
+        print(data_transformation_artifact)
 
 
     except Exception as e:
